5c3b10b1e105492675128f5b687475f83c1b44d6
Pawel Damasiewicz <pawel.damasiewicz@gmail.com>
2023-05-07 16:32:49 +0200

    Update search test with homepage link

--- tests/Controller/SearchControllerTest.php
+++ tests/Controller/SearchControllerTest.php
@@ -6,12 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class SearchControllerTest extends WebTestCase
 {
-    public function testSearchDisplaysSearchResult(): void
+    public function testDisplaysSearchResult(): void
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/games/search?query=Cyberpunk');
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h1', 'Search results for "Cyberpunk"');
+    }
+
+    public function testDisplaysLinkToHome(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/games/search');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorExists('a[href="/"]');
+        $this->assertSelectorTextContains('a.home', 'Home');
     }
 }
