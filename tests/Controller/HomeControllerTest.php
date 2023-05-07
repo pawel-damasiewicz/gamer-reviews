256edf6bd2e82aa4eafe5e6cfa1f6929e9cf2c60
Pawel Damasiewicz <pawel.damasiewicz@gmail.com>
2023-05-07 15:52:53 +0200

    Add home controller test

---
+++ tests/Controller/HomeControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeControllerTest extends WebTestCase
+{
+    public function testDisplaysWelcomeMessage(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Welcome to Gamer Reviews!');
+    }
+
+    public function testDisplaysSearchBar(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorExists('input.search-input');
+        $this->assertSelectorExists('button.search-button');
+    }
+}
