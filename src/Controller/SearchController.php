becfa2a7189d8b069cd92ca31f15f7f53098f7ff
Pawel Damasiewicz <pawel.damasiewicz@gmail.com>
2023-05-15 19:47:46 +0200

    Add displaing search results

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Game;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,12 +12,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class SearchController extends AbstractController
 {
     #[Route('/games/search', name: 'app_games_search')]
-    public function index(Request $request): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         $query = $request->query->get('query');
 
+        $games = $entityManager->getRepository(Game::class)->findByName($query);
+
         return $this->render('search/index.html.twig', [
-            'query' => $query
+            'query' => $query,
+            'games' => $games
         ]);
     }
 }
