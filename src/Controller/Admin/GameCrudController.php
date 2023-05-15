4e6c032bd19e5f871fb962d3d0710c09eb8761d0
Pawel Damasiewicz <pawel.damasiewicz@gmail.com>
2023-05-15 20:17:17 +0200

    Fix game crud controller, remove unused import

--- src/Controller/Admin/GameCrudController.php
+++ src/Controller/Admin/GameCrudController.php
@@ -4,7 +4,6 @@ namespace App\Controller\Admin;
 
 use App\Entity\Game;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class GameCrudController extends AbstractCrudController
