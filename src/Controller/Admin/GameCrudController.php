761604cd88aabca548c87f704ff4b3f0f8d03e30
Pawel Damasiewicz <pawel.damasiewicz@gmail.com>
2023-05-15 20:16:49 +0200

    Add games crud controller

---
+++ src/Controller/Admin/GameCrudController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Game;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class GameCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Game::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+            yield TextField::new('name');
+    }
+}
