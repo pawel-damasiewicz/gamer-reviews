e43d19d7e2eefdbd9c6ce98f138ee76911dcdc68
Pawel Damasiewicz <pawel.damasiewicz@gmail.com>
2023-05-16 23:06:11 +0200

    Modify home controller, change route name

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'homepage')]
+    #[Route('/', name: 'app_homepage')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
