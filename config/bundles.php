858b368ff5d2c04fa2533d95fe75a7f15de7c6a4
Pawel Damasiewicz <pawel.damasiewicz@gmail.com>
2023-05-17 15:09:29 +0200

    Add symfony/webpack-encore

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
