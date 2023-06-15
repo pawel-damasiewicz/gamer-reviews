154bb467e5e9eb4471b0ba379775d401b00738a5
Pawel Damasiewicz <pawel.damasiewicz@gmail.com>
2023-06-15 13:16:50 +0200

    Modify search controller, code style

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -12,11 +12,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class SearchController extends AbstractController
 {
     #[Route('/games/search', name: 'app_games_search')]
-    public function index(Request $request, EntityManagerInterface $entityManager): Response
-    {
+    public function index(
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response {
         $query = $request->query->get('query', '');
 
-        $games = $entityManager->getRepository(Game::class)->findByNameLike($query);
+        $games = $entityManager->getRepository(Game::class)
+                               ->findByNameLike($query);
 
         return $this->render('search/index.html.twig', [
             'query' => $query,
