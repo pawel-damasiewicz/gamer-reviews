eca0464482b9ca6bdb649b9ab723e2ddf2798585
Pawel Damasiewicz <pawel.damasiewicz@gmail.com>
2023-06-20 00:35:39 +0200

    Add review relationship to Game entity

--- src/Entity/Game.php
+++ src/Entity/Game.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\GameRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: GameRepository::class)]
@@ -18,6 +20,14 @@ class Game
 
     #[ORM\Column(type: 'float', options: ['default' => 0])]
     private ?float $trendingIndex = null;
+
+    #[ORM\OneToMany(targetEntity: Review::class, mappedBy: 'game')]
+    private $reviews;
+
+    public function __construct()
+    {
+        $this->reviews = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -41,5 +51,42 @@ class Game
         $this->trendingIndex = $trendingIndex;
 
         return $this;
+    }
+
+    public function getTrendingIndex(): float
+    {
+        return $this->trendingIndex;
+    }
+
+    public function getReviews(): Collection
+    {
+        return $this->reviews;
+    }
+
+    public function addReview(Review $review): self
+    {
+        if (!$this->reviews->contains($review)) {
+            $this->reviews->add($review);
+            $review->setGame($this);
+        }
+
+        return $this;
+    }
+
+    public function removeReview(Review $review): self
+    {
+        if ($this->reviews->contains($review)) {
+            $this->reviews->removeElement($review);
+            if ($review->getGame() === $this) {
+                $review->setGame(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return $this->name;
     }
 }
