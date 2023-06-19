8afff986f05a575d51dc808a8c8c8756251f723d
Pawel Damasiewicz <pawel.damasiewicz@gmail.com>
2023-06-20 00:33:46 +0200

    Add test game does have reviews

--- tests/Controller/GameControllerTest.php
+++ tests/Controller/GameControllerTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Controller;
 
 use App\Entity\Game;
+use App\Entity\Review;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class GameControllerTest extends WebTestCase
@@ -24,5 +25,32 @@ class GameControllerTest extends WebTestCase
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h1', 'Cyberpunk');
+    }
+
+    public function testDoesShowGameReviews(): void
+    {
+        $client = static::createClient();
+        $entityManager = static::getContainer()
+            ->get('doctrine')
+            ->getManager();
+
+        $gameRepository = $entityManager->getRepository(Game::class);
+
+        $game = $gameRepository->findOneBy(['name' => 'Cyberpunk']);
+
+        $review = new Review();
+        $review->setContent('Test Review');
+        $review->setRating(0.69);
+
+
+        $game->addReview($review);
+
+        $client->request('GET', '/game/' . $game->getId());
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains(
+            'ul#reviews li:first-child',
+            'Test Review'
+        );
     }
 }
