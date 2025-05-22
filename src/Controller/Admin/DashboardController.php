55f594345f3ee0c44a36f948a3a77a939930b795
Paweł Damasiewicz <pawel.damasiewicz@gmail.com>
2025-05-22 19:43:56 +0200

    Modify admin dashboard contr, code style

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -23,8 +23,7 @@ class DashboardController extends AbstractDashboardController
 
     public function configureDashboard(): Dashboard
     {
-        return Dashboard::new()
-            ->setTitle('App');
+        return Dashboard::new()->setTitle('App');
     }
 
     public function configureMenuItems(): iterable
